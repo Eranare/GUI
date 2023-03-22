d77946f571d028b7c7effc1530fec1d38e3c0a07
Rick <eralon18@gmail.com>
2023-03-22 13:04:55 +0100

    .

--- src/ApplicantDemo.java
+++ src/ApplicantDemo.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.json.JSONObject;
+
+import static java.nio.file.Files.createFile;
+import static java.nio.file.Files.exists;
 
 public class ApplicantDemo extends JFrame{
+
     private JPanel panelMain;
     private JLabel name;
     private JTextField nameField;
@@ -19,24 +28,71 @@ public class ApplicantDemo extends JFrame{
     btn.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            //Check input
 
-            //Show message if failed (wrong input, other errors)
+            JOptionPane.showMessageDialog(btn,"HELLO");
 
-            //Else Submit data to json?
-            // show message of success
+            writeFile();
+
+
+
+
+            // show message
         }
     });
 }
+    public static void writeFile(){
+        //Check input
+        JSONObject obj = new JSONObject();
+        //Show message if failed (wrong input, other errors)
+        try(FileWriter Data = new FileWriter("Data.JSON")) {
+            Data.write(obj.toString(4));
+            //Else Submit data to json?
+            //clear input field
 
+
+        } catch( IOException e1) {
+            e1.printStackTrace();
+            System.out.println("uhoh Something fucky went in the wucky");
+        }
+    }
     public static void main(String[] args) {
+
     ApplicantDemo h=new ApplicantDemo();
     h.setContentPane(h.panelMain);
     h.setTitle("ApplicantGUI");
-    h.setBounds(800,800 , 500, 800);
+    h.setBounds(500,200 , 700, 800);
     //h.setSize(400, 600);
     h.setVisible(true);
+    checkIfFile();
     h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void checkIfFile(){
+        if(exists(Path.of("Data.JSON"))){
+            System.out.println("Data.JSON exists");
+        }
+        else {
+
+            try {
+                createFile(Path.of("Data.JSON"));
+                JSONObject jo = new JSONObject();
+                jo.put("start of","new Json");
+                System.out.println(jo);
+                JSONObject init = new JSONObject();
+                try(FileWriter Data = new FileWriter("Data.JSON")) {
+                    Data.write(jo.toString(4));}
+                catch(IOException e1){
+                    System.out.print(" something fucky went with the wucky");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public static class Applicants {
+
+
     }
 }
 
