d501a44a7a8c5f977aaa3ef6f61a66ff762bb98b
Rick <eralon18@gmail.com>
2023-03-31 10:51:45 +0200

    more functions because why not
    delete with backup,
    Load file,
    Refresh current (somewhat reduntant),
    Load Dummy data.

--- src/ApplicantDemo.java
+++ src/ApplicantDemo.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.text.*;
 import java.awt.event.*;
 import java.io.*;
@@ -8,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
-import java.text.NumberFormat;
 
 import org.json.*;
 
@@ -36,6 +34,10 @@ public class ApplicantDemo extends JFrame {
     private JTextPane displayApplicantsField;
     private JSpinner minuteSpinner;
     private JSpinner hourSpinner;
+    private JButton Load;
+    private JButton refresh;
+    private JButton delete;
+    private JButton dummyData;
 
     private static final String DEFAULT_MESSAGE = "Please insert name and time";
 
@@ -121,7 +123,7 @@ public class ApplicantDemo extends JFrame {
     public ApplicantDemo() {
 
         super("Applicant Demo");
-
+        checkIfFile();
         loadApplicantsFromFile();
 //---------Also Chats solution
 // Add the DocumentFilter to the hourField and minuteField
@@ -178,6 +180,47 @@ public class ApplicantDemo extends JFrame {
 
         });
         displayMessage.setText(DEFAULT_MESSAGE);
+        delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Ask for confirmation
+
+                //Save file in back up file named (BackupCurrentDateTime)
+                //Delete content of main file
+                deleteFile();
+                checkIfFile();
+                displayMessage("Data deleted(Not actually gone)");
+                loadApplicantsFromFile();
+            }
+        });
+        refresh.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Reload list
+                displayMessage("Refreshing");
+                checkIfFile();
+                loadApplicantsFromFile();
+            }
+        });
+
+        Load.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Pop up window asking for which file to revert to
+                fileSelection();
+                displayMessage("File Loaded");
+                loadApplicantsFromFile();
+                //Will backup current file unless name exists already.
+
+            }
+        });
+        dummyData.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                loadDummyData();
+            }
+        });
     }
     private void displayMessage(String message) {
         // Set the text of the top text pane to the message
@@ -196,6 +239,8 @@ public class ApplicantDemo extends JFrame {
     }
 
     public static void writeFile(String name, String hour, String minute) {
+        //Check if file exists
+        checkIfFile();
         //Check input
         try {
             int hours = Integer.parseInt(hour);
@@ -234,14 +279,51 @@ public class ApplicantDemo extends JFrame {
         }
     }
 
+    public static void deleteFile() {
+        //Check input
+        try {
+
+
+            // Read existing JSON data from file
+            JSONArray jsonArray;
+            try (FileReader fileReader = new FileReader("data.json")) {
+                jsonArray = new JSONArray(new JSONTokener(fileReader));
+            } catch (IOException e) {
+                jsonArray = new JSONArray();
+            }
+
+            String currentDateTime = String.valueOf(new java.sql.Timestamp(System.currentTimeMillis()).getTime());;
+            //Create backup file
+            createFile(Path.of("Backup"+ currentDateTime+ ".JSON"));
+
+            //Write JSON data back to backup file
+            try (FileWriter fileWriter = new FileWriter("Backup"+ currentDateTime+ ".JSON")) {
+                fileWriter.write(jsonArray.toString());
+                //Delete old file
+                File oldData = new File(("data.json"));
+                oldData.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Ow boy, Good luck figuring out what happened there.");
+                return;
+            }
+
+            // JOptionPane.showMessageDialog(null, "Data saved successfully!");
+        } catch (NumberFormatException | JSONException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Something went wrong try again another day.");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Ow I hope that data was not valuable");
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
         ApplicantDemo h = new ApplicantDemo();
         h.setContentPane(h.panelMain);
         h.setTitle("ApplicantGUI");
-        h.setBounds(500, 200, 700, 600);
+        h.setBounds(500, 200, 800, 600);
         h.setVisible(true);
-        checkIfFile();
         h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -283,17 +365,98 @@ public class ApplicantDemo extends JFrame {
             try {
                 createFile(Path.of("Data.JSON"));
                 JSONObject jo = new JSONObject();
-                jo.put("start of", "new Json");
-                System.out.println(jo);
+                jo.put("[", "]");
+                //System.out.println(jo);
                 JSONObject init = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
                 try (FileWriter Data = new FileWriter("Data.JSON")) {
-                    Data.write(jo.toString(4));
+
+                    Data.write(jsonArray.toString());
                 } catch (IOException e1) {
                     System.out.print(" something fucky went with the wucky");
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+    private void fileSelection() {
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON","JSON");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("./"));
+        fileChooser.setFileFilter(filter);
+        int result = fileChooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION){
+            File selectedFile = fileChooser.getSelectedFile();
+            //Now to swap selectedFile with currentFile
+            loadFile(selectedFile);
+        }
+
+    }
+    public static void loadFile(File selectedFile) {
+        //Check if file exists
+        checkIfFile();
+        //Check input
+        try {
+
+
+
+            JSONObject values = new JSONObject();
+            //Fix structuring, look at php project
+
+            // Read existing JSON data from file
+            JSONArray jsonArray;
+            try (FileReader fileReader = new FileReader(selectedFile)) {
+                jsonArray = new JSONArray(new JSONTokener(fileReader));
+            } catch (IOException e) {
+                jsonArray = new JSONArray();
+            }
+
+
+
+            // Write Loaded File to Active Data.JSON
+            try (FileWriter fileWriter = new FileWriter("data.json")) {
+                fileWriter.write(jsonArray.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Error loading data.");
+                return;
+            }
+
+            // JOptionPane.showMessageDialog(null, "Data saved successfully!");
+        } catch (NumberFormatException | JSONException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error: Invalid input format.");
+        }
+
+    }
+    private void loadDummyData() {
+        try {
+            String jsonStr = new String(Files.readAllBytes(Paths.get("dummyData.JSON")), StandardCharsets.UTF_8);
+
+            // Create a JSONArray from the string
+            JSONArray jsonArray = new JSONArray(jsonStr);
+
+            StringBuilder formattedData = new StringBuilder();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject applicantObj = jsonArray.getJSONObject(i);
+                String name = applicantObj.getString("name ");
+                String timeString = applicantObj.getString("time ");
+
+                // Formatting time
+                DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+                Date timeDate = dateFormat.parse(timeString);
+                String time = dateFormat.format(timeDate);
+
+                // Formatting
+                formattedData.append("Applicant name: ").append(name).append(", timeslot: ").append(time).append("\n");
+            }
+
+            // Display the formatted data in the JTextPane
+            displayApplicantsField.setText(formattedData.toString());
+            displayMessage("Showing Dummy Data");
+        } catch (IOException | JSONException | ParseException e) {
+            e.printStackTrace();
         }
     }
 }
